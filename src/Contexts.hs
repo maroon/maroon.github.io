24dd42371d2b70a1ff5ac33c56ec64d9c6c8be1a
Ryan Maroon <ryan.maroon@protonmail.com>
2019-06-29 14:23:50 -0500

    Move contexts into a separate module

---
+++ src/Contexts.hs
@@ -0,0 +1,45 @@
+module Contexts
+  ( metaContext
+  , postContext
+  ) where
+
+import Config
+import Hakyll
+import System.FilePath
+  ( takeDirectory
+  , splitFileName
+  , addTrailingPathSeparator
+  )
+
+maybeField :: String -> Maybe String -> Context a
+maybeField _ Nothing    = mempty
+maybeField key (Just v) = constField key v
+
+formatPostUrlContext :: Context a
+formatPostUrlContext = mapContext format (urlField "url")
+  where
+    format url =
+      case splitFileName url of
+        (p, "index.html") -> addTrailingPathSeparator . takeDirectory $ p
+        _                 -> url
+
+metaContext :: Config -> Context String
+metaContext config =
+  constField "site_title" (title config) <>
+  constField "site_desc" (description config) <>
+  constField "name" (author . social $ config) <>
+  constField "email" (email . social $ config) <>
+  maybeField "github" (github . social $ config) <>
+  maybeField "twitter" (twitter . social $ config) <>
+  boolField "show_feed" (const . showFeed . display $ config) <>
+  boolField "show_email" (const . showEmail . display $ config) <>
+  boolField "show_github" (const . showGithub . display $ config) <>
+  boolField "show_twitter" (const . showTwitter . display $ config) <>
+  defaultContext
+
+postContext :: Config -> Context String
+postContext config =
+  boolField "is_post" (const True) <>
+  dateField "date" "%b %e, %Y" <>
+  formatPostUrlContext <>
+  metaContext config
--- src/Site.hs
+++ src/Site.hs
@@ -3,12 +3,12 @@
 
 import Archive
 import Config
+import Contexts
 import Data.Monoid ((<>))
 import Feed
 import Hakyll
 import ScssCompiler
-import System.FilePath ((</>), dropExtension, takeDirectory,
-                        splitFileName, addTrailingPathSeparator)
+import System.FilePath ((</>), dropExtension)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -115,38 +115,3 @@ indexRoute :: Routes
 indexRoute =
   customRoute ((</> "index") . dropExtension . toFilePath) `composeRoutes`
   setExtension "html"
-
-
--- Contexts --------------------------------------------------------------------
-maybeField :: String -> Maybe String -> Context a
-maybeField _ Nothing    = mempty
-maybeField key (Just v) = constField key v
-
-formatPostUrlContext :: Context a
-formatPostUrlContext = mapContext format (urlField "url")
-  where
-    format url =
-      case splitFileName url of
-        (p, "index.html") -> addTrailingPathSeparator . takeDirectory $ p
-        _                 -> url
-
-metaContext :: Config -> Context String
-metaContext config =
-  constField "site_title" (title config) <>
-  constField "site_desc" (description config) <>
-  constField "name" (author . social $ config) <>
-  constField "email" (email . social $ config) <>
-  maybeField "github" (github . social $ config) <>
-  maybeField "twitter" (twitter . social $ config) <>
-  boolField "show_feed" (const . showFeed . display $ config) <>
-  boolField "show_email" (const . showEmail . display $ config) <>
-  boolField "show_github" (const . showGithub . display $ config) <>
-  boolField "show_twitter" (const . showTwitter . display $ config) <>
-  defaultContext
-
-postContext :: Config -> Context String
-postContext config =
-  boolField "is_post" (const True) <>
-  dateField "date" "%b %e, %Y" <>
-  formatPostUrlContext <>
-  metaContext config
