ccc4ec061eb8a7473a4435d44e74ec8da8597fa3
Ryan Maroon <ryan.maroon@protonmail.com>
2019-06-29 17:04:11 -0500

    Refactor routes for better formatting

--- src/Routes.hs
+++ src/Routes.hs
@@ -8,17 +8,23 @@ import Hakyll
 import System.FilePath ((</>), dropExtension)
 
 dateRoute :: Routes
-dateRoute =
-  siteRoute `composeRoutes`
-  gsubRoute "/[0-9]{4}-[0-9]{2}-[0-9]{2}-" (replaceAll "-" $ const "/") `composeRoutes`
-  indexRoute
+dateRoute = foldl1 composeRoutes routes
+  where
+    pattern = "/[0-9]{4}-[0-9]{2}-[0-9]{2}-"
+    routes  =
+      [ siteRoute
+      , gsubRoute pattern (replaceAll "-" $ const "/")
+      , indexRoute
+      ]
 
 indexRoute :: Routes
-indexRoute =
-  siteRoute `composeRoutes`
-  customRoute ((</> "index") . dropExtension . toFilePath) `composeRoutes`
-  setExtension "html"
+indexRoute = foldl1 composeRoutes routes
+  where
+    routes =
+      [ siteRoute
+      , customRoute ((</> "index") . dropExtension . toFilePath)
+      , setExtension "html"
+      ]
 
 siteRoute :: Routes
-siteRoute =
-  gsubRoute "site/" $ const ""
+siteRoute = gsubRoute "site/" $ const ""
