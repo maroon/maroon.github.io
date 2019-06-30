0425e89da18b37b31f4a2a7e2f3e32bf5c700526
Ryan Maroon <ryan.maroon@protonmail.com>
2019-06-30 08:21:45 -0500

    Add configuration and remove site paths and route

--- src/Site.hs
+++ src/Site.hs
@@ -12,7 +12,7 @@ import Config (Config, loadConfiguration)
 import Data.Monoid ((<>))
 import Feed (feedConfig)
 import Hakyll
-import Routes (dateRoute, indexRoute, siteRoute)
+import Routes (dateRoute, indexRoute)
 import ScssCompiler (scssCompiler)
 import qualified Config as C (display, postsPerPage)
 
@@ -20,45 +20,50 @@ main :: IO ()
 main = loadConfiguration >>= runHakyll
 
 runHakyll :: Config -> IO ()
-runHakyll config = hakyll $ do
+runHakyll config = do
+  let cfg = defaultConfiguration { providerDirectory = "site/" }
+  hakyllWith cfg $ hakyllRules config
+
+hakyllRules :: Config -> Rules ()
+hakyllRules config = do
   let postCtx = postContext config
   let metaCtx = metaContext config
   let postsPerPage = C.postsPerPage . C.display $ config
 
-  match "site/assets/**" $ do
-    route siteRoute
+  match "assets/**" $ do
+    route idRoute
     compile copyFileCompiler
 
-  match "site/scss/**.scss" $ do
+  match "scss/**.scss" $ do
     compile getResourceString
 
-  scssDependency <- makePatternDependency "site/scss/**.scss"
+  scssDependency <- makePatternDependency "scss/**.scss"
   rulesExtraDependencies [scssDependency] $ do
     create ["assets/main.css"] $ do
-      route siteRoute
+      route idRoute
       compile $
-        loadBody "site/scss/novella.scss"
+        loadBody "scss/novella.scss"
           >>= makeItem
           >>= scssCompiler
 
-  match "site/about.md" $ do
+  match "about.md" $ do
     route indexRoute
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "site/templates/post/body.html" postCtx
-      >>= loadAndApplyTemplate "site/templates/site/body.html" postCtx
+      >>= loadAndApplyTemplate "templates/post/body.html" postCtx
+      >>= loadAndApplyTemplate "templates/site/body.html" postCtx
       >>= relativizeUrls
 
-  match "site/posts/*" $ do
+  match "posts/*" $ do
     route dateRoute
     compile $ pandocCompiler
       >>= saveSnapshot "content"
-      >>= loadAndApplyTemplate "site/templates/post/body.html" postCtx
-      >>= loadAndApplyTemplate "site/templates/site/body.html" postCtx
+      >>= loadAndApplyTemplate "templates/post/body.html" postCtx
+      >>= loadAndApplyTemplate "templates/site/body.html" postCtx
       >>= relativizeUrls
 
   page <- buildArchivePaginateWith archiveGroup "posts/*" archiveId
   paginateRules page $ \pageNum pattern -> do
-    route siteRoute
+    route idRoute
     compile $ do
       posts <- recentFirst =<< loadAll pattern
       let paginateCtx = archiveContext page pageNum
@@ -69,14 +74,14 @@ runHakyll config = hakyll $ do
             postCtx
 
       makeItem ""
-        >>= loadAndApplyTemplate "site/templates/archive/body.html" ctx
-        >>= loadAndApplyTemplate "site/templates/site/body.html" ctx
+        >>= loadAndApplyTemplate "templates/archive/body.html" ctx
+        >>= loadAndApplyTemplate "templates/site/body.html" ctx
         >>= relativizeUrls
 
-  match "site/index.html" $ do
-    route siteRoute
+  match "index.html" $ do
+    route idRoute
     compile $ do
-      posts <- recentFirst =<< loadAll "site/posts/*"
+      posts <- recentFirst =<< loadAll "posts/*"
       archiveYear <- yearForIdentifier (itemIdentifier . head $ posts)
       let archivePage = show . archiveId $ archiveYear
       let indexCtx =
@@ -88,19 +93,19 @@ runHakyll config = hakyll $ do
 
       getResourceBody
         >>= applyAsTemplate indexCtx
-        >>= loadAndApplyTemplate "site/templates/site/body.html" indexCtx
+        >>= loadAndApplyTemplate "templates/site/body.html" indexCtx
         >>= relativizeUrls
 
-  match "site/templates/**" $ compile templateBodyCompiler
+  match "templates/**" $ compile templateBodyCompiler
 
-  create ["site/feed.xml"] $ do
-    route siteRoute
+  create ["feed.xml"] $ do
+    route idRoute
     compile $ do
       let feedCtx =
             bodyField "description" <>
             postCtx
 
       posts <- fmap (take postsPerPage) . recentFirst
-        =<< loadAllSnapshots "site/posts/*" "content"
+        =<< loadAllSnapshots "posts/*" "content"
 
       renderAtom (feedConfig config) feedCtx posts
