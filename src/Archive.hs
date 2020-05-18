607cb6556f2176cb8f2f5e7bc3d8aafacf52d20a
Ryan Maroon <ryan.maroon@protonmail.com>
2020-05-17 20:31:21 -0700

    Add MonadFail constraint to several functions

--- src/Archive.hs
+++ src/Archive.hs
@@ -8,6 +8,7 @@ module Archive
 
 import Control.Applicative (empty)
 import Control.Monad (forM)
+import Control.Monad.Fail (MonadFail)
 import Data.Bool (bool)
 import Data.List (groupBy)
 import Data.Time.Calendar (toGregorian)
@@ -17,27 +18,35 @@ import Hakyll
 import qualified Data.Map as M
 import qualified Data.Set as S
 
-yearForIdentifier :: (MonadMetadata m, Num a) => Identifier -> m a
+yearForIdentifier :: (MonadMetadata m, MonadFail m, Num a)
+                  => Identifier
+                  -> m a
 yearForIdentifier i = do
   time <- getItemUTC defaultTimeLocale i
   let (year, _, _) = toGregorian . localDay $ utcToLocalTime utc time
   return $ fromIntegral year
 
-archiveGroup :: MonadMetadata m => [Identifier] -> m [[Identifier]]
+archiveGroup :: (MonadMetadata m, MonadFail m)
+             => [Identifier]
+             -> m [[Identifier]]
 archiveGroup ids = do
   sorted <- mapM yearMap =<< sortRecentFirst ids
   return $ (fmap snd) <$> groupBy (\a b -> fst a == fst b) sorted
   where
-    yearMap :: MonadMetadata m => Identifier -> m (Integer, Identifier)
+    yearMap :: (MonadMetadata m, MonadFail m)
+            => Identifier
+            -> m (Integer, Identifier)
     yearMap i = yearForIdentifier i >>= \year -> return (year, i)
 
 archiveId :: PageNumber -> Identifier
 archiveId pageNum = fromFilePath $ "archive/year/" ++ (show pageNum) ++ "/index.html"
 
-archivePages :: MonadMetadata m => [Identifier] -> m PageNumber
+archivePages :: (MonadMetadata m, MonadFail m)
+             => [Identifier]
+             -> m PageNumber
 archivePages = yearForIdentifier . head
 
-buildArchivePaginateWith :: MonadMetadata m
+buildArchivePaginateWith :: (MonadMetadata m, MonadFail m)
                          => ([Identifier] -> m [[Identifier]])
                          -> Pattern
                          -> (PageNumber -> Identifier)
